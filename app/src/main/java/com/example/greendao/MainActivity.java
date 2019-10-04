0abfb181a9c379f1aeabd65526b41dddb4d7d357
mahdiHT <mht0440232@gmail.com>
2019-10-04 11:56:51 +0330

    Find widget and create recycler items

--- app/src/main/java/com/example/greendao/MainActivity.java
+++ app/src/main/java/com/example/greendao/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.greendao;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText_userName;
+    private EditText editText_userId;
+    private EditText editText_note;
+    private Button button_addUser;
+    private Button button_addNote;
+
+    private RecyclerView recyclerView_user;
+    private RecyclerView recyclerView_note;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText_userName = findViewById(R.id.editText_user_name);
+        editText_userId = findViewById(R.id.editText_user_id);
+        editText_note = findViewById(R.id.editText_note);
+        button_addUser = findViewById(R.id.button_add_user);
+        button_addNote = findViewById(R.id.button_add_note);
+        recyclerView_user = findViewById(R.id.recyclerView_user);
+        recyclerView_note = findViewById(R.id.recyclerView_note);
+
     }
 }
